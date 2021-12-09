cfb6cbc7c6e6371e7d608d07d5052ff1411f75e7
Mahalingam97 <91451945+Mahalingam97@users.noreply.github.com>
2021-12-10 00:20:23 +0530

    Delete 4.12.2021 directory

--- 4.12.2021/Node_sequelize/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const {moviePayment,orderMovie}=require("../controllers/paymentController");
-
-const payrouter=require("express").Router();
-
-payrouter.post("/payment",moviePayment)
-payrouter.get("/order/:productId",orderMovie)
-
-module.exports=payrouter
